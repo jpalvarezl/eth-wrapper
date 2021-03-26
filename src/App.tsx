ad65a22cee04789623c1047a836e42f299b347bf
Jose Alvarez <jp.alvarezl@gmail.com>
2021-03-26 11:45:50 +0100

    Added icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import styled from 'styled-components';
 import EthBalance from './components/EthBalance';
 import WethBalance from './components/WethBalance';
 import Wrapper from './components/Wrapper';
-import { CardContent, Card, makeStyles, CardActions } from '@material-ui/core';
-import { Title } from '@gnosis.pm/safe-react-components';
+import { CardContent, Card, makeStyles, CardActions, CardHeader, IconButton, Avatar } from '@material-ui/core';
+import { ReactComponent as EthLogo } from './assets/svg/eth.svg';
+import { ReactComponent as WethLogo } from './assets/svg/weth.svg';
 import React, { useState } from 'react';
 
 const Container = styled.form`
@@ -22,6 +23,10 @@ const useStyles = makeStyles({
     maxWidth: "300px",
     maxHeight: "300px"
   },
+  logo: {
+    width: "32px",
+    height: "32px",
+  }
 });
 
 
@@ -30,9 +35,19 @@ const App: React.FC = () => {
   const [wrap, setWrap] = useState(true);
 
   return (
-    <Card className={classes.root} onClick={() => setWrap(!wrap)} >
+    <Card className={classes.root}  >
+      <CardHeader
+        avatar={
+
+          wrap ?
+            <Avatar alt="wrap" > <EthLogo className={classes.logo} /></Avatar> :
+            <Avatar alt="unwrap" ><WethLogo className={classes.logo} /></Avatar>
+        }
+        title={wrap ? "WRAP" : "UNWRAP"}
+        subheader="Tap to toggle (un)wrap"
+        onClick={() => setWrap(!wrap)}
+      />
       <CardContent>
-        <Title size="lg">{wrap ? "WRAP" : "UNWRAP"}</Title>
         {wrap ?
           <EthBalance />
           : <WethBalance />}
@@ -40,7 +55,7 @@ const App: React.FC = () => {
       <CardActions>
         <Wrapper wrap={wrap} />
       </CardActions>
-    </Card>
+    </Card >
   );
 };
 
